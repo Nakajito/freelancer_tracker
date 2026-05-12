778b22bc9d8df4dea508c9030bb0a2e53faa594b
Daniel <nakajito@proton.me>
2026-05-12 11:34:21 -0500

    fix(icons): update path to icons

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -14,7 +14,9 @@ DATABASES = {
 INSTALLED_APPS = [*INSTALLED_APPS, "debug_toolbar"]  # noqa: F405
 
 MIDDLEWARE = [
-    *MIDDLEWARE,  # noqa: F405
+    m for m in MIDDLEWARE  # noqa: F405
+    if m != "whitenoise.middleware.WhiteNoiseMiddleware"
+] + [
     "allauth.account.middleware.AccountMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
